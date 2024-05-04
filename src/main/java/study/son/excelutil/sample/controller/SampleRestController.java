3f82dd621f11985e96e90f57c26fdd996542bf41
JH <dev.son.jh@gmail.com>
2024-05-05 00:21:45 +0900

    feat: added sample download API, service.

--- src/main/java/study/son/excelutil/sample/controller/SampleRestController.java
+++ src/main/java/study/son/excelutil/sample/controller/SampleRestController.java
@@ -1,4 +1,36 @@
 package study.son.excelutil.sample.controller;
 
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.son.excelutil.sample.service.SampleDownloadService;
+import java.io.ByteArrayOutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@RestController
 public class SampleRestController {
+    private final SampleDownloadService sampleDownloadService;
+
+    public SampleRestController(SampleDownloadService sampleDownloadService) {
+        this.sampleDownloadService = sampleDownloadService;
+    }
+
+    @PostMapping("/samples/download")
+    public ResponseEntity<ByteArrayOutputStream> downloadExcel() {
+        ByteArrayOutputStream byteArrayOutputStream = sampleDownloadService.downloadExcel();
+        return ResponseEntity.ok().headers(createHeaders()).body(byteArrayOutputStream);
+    }
+
+    private HttpHeaders createHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        ContentDisposition contentDisposition = ContentDisposition.builder("attachment")
+                .filename(URLEncoder.encode("downloadFilename", StandardCharsets.UTF_8))
+                .build()
+                ;
+        headers.setContentDisposition(contentDisposition);
+        return headers;
+    }
 }
--- src/main/java/study/son/excelutil/sample/service/SampleDownloadService.java
+++ src/main/java/study/son/excelutil/sample/service/SampleDownloadService.java
@@ -1,4 +1,43 @@
 package study.son.excelutil.sample.service;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+import study.son.excelutil.sample.dto.ExcelDownloadDto;
+import study.son.excelutil.util.excel.writer.ExcelWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class SampleDownloadService {
+    public ByteArrayOutputStream downloadExcel() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        List<ExcelDownloadDto> data = getSampleData();
+        ExcelWriter writer = new ExcelWriter(ExcelDownloadDto.class);
+        XSSFWorkbook workbook = writer.writeExcel(new XSSFWorkbook(), data);
+        try {
+            workbook.write(byteArrayOutputStream);
+            byteArrayOutputStream.flush();
+            byteArrayOutputStream.close();
+        } catch (IOException e) {
+            e.getLocalizedMessage();
+        }
+        return byteArrayOutputStream;
+    }
+
+    private List<ExcelDownloadDto> getSampleData() {
+        List<ExcelDownloadDto> dataList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            dataList.add(ExcelDownloadDto.builder()
+                    .number(i)
+                    .content("content")
+                    .createdDateTime(LocalDateTime.now())
+                    .build()
+            );
+        }
+        return dataList;
+    }
 }
